0dc9eb877a20bf35f616926ff1980de62c407c04
Arnaud Guigue Billon <77971546+Tauste73@users.noreply.github.com>
2022-09-28 09:12:29 +0200

    Premier push
    Menu

---
+++ babayagaMobile/android/app/src/main/kotlin/com/example/babayagamobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.babayagamobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ babayagaMobile/lib/homescreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+        decoration: const BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage("asset/images/personnages/babayaga.png"),
+    fit: BoxFit.cover,
+    ),
+    ),
+    constraints: BoxConstraints.expand(),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              print("YO");
+            },
+            child: Text("babayaga")
+        ),
+        ElevatedButton(
+            onPressed: () {
+              print("YOOO");
+            },
+            child: Text("YO")
+        )
+      ],
+
+
+    ),
+
+
+
+    )
+    );
+
+  }
+}
